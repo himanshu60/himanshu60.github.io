ad5031d08141a2eb0778c2f324b7c3dd49a610ee
himanshu raghuvanshi <baliyanh10@gmail.com>
2023-04-13 22:07:58 +0530

    portfolio

---
+++ scripts/download.js
@@ -0,0 +1,12 @@
+const resume_btn1 = document.querySelector("#resume-link-1");
+const resume_btn2 = document.querySelector("#resume-link-2");
+
+
+resume_btn2.addEventListener("click", () => {
+    window.location.assign("https://drive.google.com/file/d/1vBwRdoqf73gkwaBrB0If34oQ0RwP4Wtc/view?usp=sharing", "_blank");
+})
+
+document.getElementById('resume-link-1').addEventListener("click", () => {
+    window.location.assign("https://drive.google.com/file/d/1vBwRdoqf73gkwaBrB0If34oQ0RwP4Wtc/view?usp=sharing", "_blank");
+
+})
