248e302cf017a181818a1767e223067add02422c
himanshu raghuvanshi <baliyanh10@gmail.com>
2023-06-05 22:40:41 +0530

    resume link update

--- scripts/download.js
+++ scripts/download.js
@@ -3,11 +3,11 @@ const resume_btn2 = document.querySelector("#resume-link-2");
 
 
 resume_btn2.addEventListener("click", () => {
-    window.location.assign("https://drive.google.com/file/d/1vBwRdoqf73gkwaBrB0If34oQ0RwP4Wtc/view?usp=sharing", "_blank");
+    window.location.assign("https://drive.google.com/drive/folders/17Dkyct_ZXiNa1jJ-evfEFf4olzPgj-Sq?usp=sharing", "_blank");
 })
 
 resume_btn1.addEventListener("click", () => {
-    window.location.assign("https://drive.google.com/file/d/1vBwRdoqf73gkwaBrB0If34oQ0RwP4Wtc/view?usp=sharing", "_blank");
+    window.location.assign("https://drive.google.com/drive/folders/17Dkyct_ZXiNa1jJ-evfEFf4olzPgj-Sq?usp=sharing", "_blank");
 })
 
 // document.getElementById('resume-link-1').addEventListener("click", () => {
